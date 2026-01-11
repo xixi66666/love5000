5758d591d0d2d9686a2b7d3c0854d36d77661ab4
xixixiaozi <1253324157@qq.com>
2026-01-11 22:51:38 +0800

    01.11-创建项目框架

---
+++ src/main/java/com/example/love530/Love530Application.java
@@ -0,0 +1,13 @@
+package com.example.love530;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Love530Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Love530Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/love530/Love530ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.love530;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Love530ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
