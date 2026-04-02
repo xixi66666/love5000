8304b98002ad7d745ebf29abc93b474953406d25
xixixiaozi <1253324157@qq.com>
2026-04-02 21:21:25 +0800

    feat: 把lovestory微服务整合进主项目Git仓库

---
+++ lovestory/src/main/java/com/ycxandwuqian/love/LovestoryApplication.java
@@ -0,0 +1,13 @@
+package com.ycxandwuqian.love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovestoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovestoryApplication.class, args);
+    }
+
+}
---
+++ lovestory/src/test/java/com/ycxandwuqian/love/LovestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycxandwuqian.love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovestoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
