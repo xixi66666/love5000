f69333ad1d14db9c9c415b5837ec34df8a67f207
YangChenxi <1253324157@qq.com>
2026-04-24 20:32:27 +0800

    0424

---
+++ lovestory/src/main/java/com/ycxandwuqian/love/service/uploadPhotoService.java
@@ -0,0 +1,10 @@
+package com.ycxandwuqian.love.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+public interface uploadPhotoService {
+    void uploadPhoto();
+}
---
+++ lovestory/src/main/java/com/ycxandwuqian/love/service/uploadPhotoServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ycxandwuqian.love.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author YangChenxi
+ * @CreateTime 2026-04-22
+ */
+@Service
+public class uploadPhotoServiceImpl implements uploadPhotoService{
+
+    @Override
+    public void uploadPhoto() {
+
+    }
+}
